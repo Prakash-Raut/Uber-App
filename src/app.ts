47a0bea05fd6598a1b5a13c316f2546258f4e46a
Prakash Raut <prakash_raut@outlook.com>
2024-08-09 15:58:52 +0530

    feat: add mock driver app interface sample env

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { join } from "node:path";
 import { v1Router } from "./routes";
 
@@ -9,5 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", v1Router);
+
+app.get("/", (_: Request, res: Response) => {
+	res.sendFile(join(__dirname, "public", "index.html"));
+});
 
 export { app };
